ec47a9be9ff81252f7a27e43527f2b4a0440ffb3
Yaowen Xu <43050345+alexxuyaowen@users.noreply.github.com>
2022-05-27 13:39:47 -0700

    done

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const userInput = new URLSearchParams(window.location.search).get("search");
 const albums =document.getElementById("albums");
 const searchIcon = document.getElementById("search-icon");
 
+const loader = document.querySelector('.loader');
+
 let resultInfo = document.getElementById("result-info");
 
-
-
+resultInfo.style.display="none";
+loader.style.display="inline";
 
 fetchJsonp(`https://itunes.apple.com/search?term=${userInput}&media=music&entity=album&attribute=artistTerm&limit=200`)
 .then(res => res.json())
 .then(data => {
   resultInfo.innerHTML = `${data.resultCount} results for "${userInput}"`;
+
+  resultInfo.style.display="inline";
+  loader.style.display="none";
 
   data.results.forEach(e => albums.innerHTML += `<div class="album">
     <img src=${e.artworkUrl100} class="cover-pic">
