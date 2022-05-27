73891ab34c0eafc923a35edb9a414cf1af840df9
Yaowen Xu <43050345+alexxuyaowen@users.noreply.github.com>
2022-05-27 13:55:38 -0700

    +

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const userInput = new URLSearchParams(window.location.search).get("search");
 
-const albums =document.getElementById("albums");
+const albums = document.getElementById("albums");
 const searchIcon = document.getElementById("search-icon");
 
 const loader = document.querySelector('.loader');
 
 let resultInfo = document.getElementById("result-info");
 
-resultInfo.style.display="none";
-loader.style.display="inline";
+if (userInput !== null) {
+  resultInfo.style.display = "none";
+  loader.style.display = "inline";
 
-fetchJsonp(`https://itunes.apple.com/search?term=${userInput}&media=music&entity=album&attribute=artistTerm&limit=200`)
-.then(res => res.json())
-.then(data => {
-  resultInfo.style.display="inline";
-  loader.style.display="none";
+  fetchJsonp(`https://itunes.apple.com/search?term=${userInput}&media=music&entity=album&attribute=artistTerm&limit=200`)
+    .then(res => res.json())
+    .then(data => {
+      resultInfo.style.display = "inline";
+      loader.style.display = "none";
 
-  if (userInput === null) return;
-  resultInfo.innerHTML = `${data.resultCount} results for "${userInput}"`;
+      resultInfo.innerHTML = `${data.resultCount} results for "${userInput}"`;
 
-  data.results.forEach(e => albums.innerHTML += `<div class="album">
+      data.results.forEach(e => albums.innerHTML += `<div class="album">
     <img src=${e.artworkUrl100} class="cover-pic">
     <p class="collection-name">${e.collectionName}</p>
   </div>`);
 
-})
+    })
+}
